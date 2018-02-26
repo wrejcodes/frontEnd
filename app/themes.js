13d34820ec038b45c6915370f46d49a3f2b45b83
bsonge <w909781@usm.edu>
2018-02-25 19:24:18 -0600

    aah

---
+++ app/themes.js
@@ -0,0 +1,67 @@
+// // // The idea for this file is to check and set active in themes
+// //
+// // // SET ACTIVE THEME
+// // // GET ACTIVE THEME
+// //
+// // const setActive = (theme)=>{
+// //
+// // }
+// // const getActive = ()=>{
+// //   for(var theme in themes){
+// //     if(theme.active) return theme;
+// //     else {
+// //
+// //     }
+// //   }
+// // }
+// //
+// // const defaultTheme = ()=>{
+// //   this.active = True;
+// //   this.primary = 'white';
+// //   this.secondary = '#d3d3d3';
+// //   this.tertiary = '#5b5b5b';
+// //   this.header_text = 'black';
+// //   this.primary_text = 'black';
+// //   this.secondary_text = '#5b5b5b';
+// // }
+// //
+// // const themes = {
+// //   default: defaultTheme.apply(this, arguments),
+// //   blue: {
+// //     active: False,
+// //     primary: 'white',
+// //     secondary: '#a2d5e5',
+// //     tertiary: '#9fb2db',
+// //     header_text: 'white',
+// //     primary_text: 'black',
+// //     secondary_text: 'white',
+// //   },
+// // };
+//
+// const themes = {
+//   default: {
+//     active: true,
+//     primary: 'white',
+//     secondary: '#d3d3d3',
+//     tertiary: '#5b5b5b',
+//     header_text: 'black',
+//     primary_text: 'black',
+//     secondary_text: '#5b5b5b',
+//   },
+//   blue: {
+//     // ...default,
+//     active: false,
+//     // primary: 'white',
+//     secondary: '#a2d5e5',
+//     tertiary: '#9fb2db',
+//     header_text: 'white',
+//     // primary_text: 'black',
+//     secondary_text: 'white',
+//   },
+// };
+//
+//
+//
+// export{
+//   themes,
+// };
