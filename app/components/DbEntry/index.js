83af11eb18e192b7b7a3e3e45cc4c9cb5d0cefa6
bsonge <w909781@usm.edu>
2018-03-07 00:34:37 -0600

    db view refinement

--- app/components/DbEntry/index.js
+++ app/components/DbEntry/index.js
@@ -20,7 +20,7 @@ class DbEntry extends React.Component { // eslint-disable-line react/prefer-stat
       border: 1px solid black;
       margin:auto;
       width:100%;
-      height:12.4%;
+      height:auto;
       background-color: ${theme.get('tertiary')};
       color: $(theme.get('secondary_text'));
       position:relative;
@@ -33,8 +33,8 @@ class DbEntry extends React.Component { // eslint-disable-line react/prefer-stat
     return (
       <Entry>
         <h3> {this.props.moch.assay_name} :
-          <span style={{ fontSize: '.9em' }}> ID: {this.props.moch.aid} </span></h3>
-        <p> Description: {this.props.moch.assay_desc}</p>
+          <span style={{ fontSize: '.9em' }}> ID: {this.props.moch.aeid} </span></h3>
+        <p style={{ paddingTop: '.5em' }}> Description: {this.props.moch.assay_component_endpoint_desc}</p>
       </Entry>
     );
   }
