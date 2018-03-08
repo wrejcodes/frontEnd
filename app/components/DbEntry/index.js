0bbe8f2f517f82a6a0825361907fee06c51d8df4
bsonge <w909781@usm.edu>
2018-03-08 09:17:10 -0600

    added entries

--- app/components/DbEntry/index.js
+++ app/components/DbEntry/index.js
@@ -10,10 +10,20 @@ import { getActive } from 'themes';
 import PropTypes from 'prop-types';
 
 class DbEntry extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  // constructor(props){
-  //   super(props);
-  //   this.setState();
-  // }
+  constructor(props) {
+    super(props);
+    this.minmax = this.minmax.bind(this);
+    this.state = { toggle: false };
+  }
+  minmax(evt) {
+    evt.preventDefault();
+    // this.setState({ toggle: !this.state.toggle });
+  }
+  fill() {
+
+  }
+  mochEntries = Object.entries(this.props.moch);
+  mochComponents = this.mochEntries.map((mochEntry) => <p style={{ display: 'inline-block', padding: '10px' }} > {mochEntry[0]}: {mochEntry[1]}</p>);
   render() {
     const theme = getActive();
     const Entry = styled.div`
@@ -30,8 +40,12 @@ class DbEntry extends React.Component { // eslint-disable-line react/prefer-stat
         margin: 0px;
       }
     `;
+    // const open = {
+    //   height: '100%',
+    //   display: 'block',
+    // };
     return (
-      <Entry>
+      <Entry onClick={(evt) => this.minmax(evt)} >
         <h3> {this.props.moch.assay_name} :
           <span style={{ fontSize: '.9em' }}> ID: {this.props.moch.aeid} </span></h3>
         <p style={{ paddingTop: '.5em' }}> Description: {this.props.moch.assay_component_endpoint_desc}</p>
--- app/containers/EntriesPage/index.js
+++ app/containers/EntriesPage/index.js
@@ -48,8 +48,13 @@ export class EntriesPage extends React.Component { // eslint-disable-line react/
         <Select onChange={this.changeSelection}>
           <option value="blank" hidden>Select Form</option>
           <option value="target">Target</option>
+          <option value="target_id">Target ID</option>
           <option value="experiment">Experiment</option>
           <option value="chemical">Chemical</option>
+          <option value="toxicity">Toxicity</option>
+          <option value="assay">Assay</option>
+          <option value="citation">Citation</option>
+          <option value="citation_id">Citation ID</option>
         </Select>
         <Form schema={SCHEMAS[this.state.selection]} />
       </div>
