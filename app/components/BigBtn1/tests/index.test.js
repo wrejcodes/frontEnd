49492b3504d0e596f4c0bca9c4a5a05baa366488
justinnunez1717 <justinnunez1717@gmail.com>
2018-04-02 17:20:09 -0500

    Basic tests for <BigBtn1 /> component and <HomePage /> container

--- app/components/BigBtn1/tests/index.test.js
+++ app/components/BigBtn1/tests/index.test.js
@@ -1,39 +1,58 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import * as matchers from 'jest-immutable-matchers';
+import { getActive } from 'themes';
+import { Map } from 'immutable';
 import BigBtn1 from '../index';
 
+
 describe('<BigBtn1 />', () => {
+  beforeEach(() => {
+    jest.addMatchers(matchers);
+  });
+
   it('Expect to render one styled button', () => {
-    const theme = { color: 'red' };
+    const theme = getActive();
     const wrapper = shallow(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
-    expect(wrapper.find('a').length).toEqual(1);
+    expect(wrapper.find('.button').length).toEqual(1);
   });
 
   it('Expect to receieve a link from props', () => {
-    const wrapper = shallow(<BigBtn1 link="/database" handler={(link) => this.navigate(link)} />);
+    const theme = getActive();
+    const wrapper = shallow(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
     expect(wrapper.instance().props.link).toEqual('/database');
   });
-  // it('Expect to receieve text from props', () => {
-  //   const wrapper = shallow(<BigBtn1 text="Home" />);
-  //   expect(wrapper.instance().props.text).toEqual('Home');
-  // });
-  //
-  // it('Expect to receieve a theme from props', () => {
-  //   const theme = { color: 'red' };
-  //   const wrapper = shallow(<BigBtn1 theme={theme} />);
-  //   expect(wrapper.instance().props.theme.color).toEqual('red');
-  // });
-  //
-  // it('Expect to receieve a tab from props', () => {
-  //   const tab = '5px';
-  //   const wrapper = shallow(<BigBtn1 tab={tab} />);
-  //   expect(wrapper.instance().props.tab).toEqual('5px');
-  // });
-  //
+
+  it('Expect to receieve text from props', () => {
+    const theme = getActive();
+    const wrapper = shallow(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
+    expect(wrapper.instance().props.text).toEqual('View Database');
+  });
+
+  it('Expect to receieve a theme from props', () => {
+    const theme = getActive();
+    const wrapper = shallow(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
+    expect(wrapper.instance().props.theme).toEqual(Map({
+      active: expect.any(Boolean),
+      primary: expect.any(String),
+      secondary: expect.any(String),
+      tertiary: expect.any(String),
+      four: expect.any(String),
+      header_text: expect.any(String),
+      primary_text: expect.any(String),
+      secondary_text: expect.any(String),
+    }));
+  });
+
+  it('Expect to receieve a tab from props', () => {
+    const theme = getActive();
+    const wrapper = shallow(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
+    expect(wrapper.instance().props.tab).toEqual('1%');
+  });
+
   // it('Expect to receieve a handler from props', () => {
-  //   const handler = () => { };
-  //   const wrapper = shallow(<BigBtn1 handler={handler} />);
-  //   expect(wrapper.find('handler'));
+  //   const theme = getActive();
+  //   const wrapper = mount(<BigBtn1 tab="1%" theme={theme} text="View Database" link="/database" handler={(link) => this.navigate(link)} />);
+  //   expect(wrapper.instance().props.link).toEqual('/database');
   // });
 });
