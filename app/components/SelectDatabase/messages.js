955a5f5ed042832676cf47f145a48f237b796505
bsonge <w909781@usm.edu>
2018-04-08 01:00:01 -0500

    Changed SelectDatabase/messages.js to proper format, updated SelectDatabase/index.js to reflect the changes.

--- app/components/SelectDatabase/messages.js
+++ app/components/SelectDatabase/messages.js
@@ -6,16 +6,47 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  Values: {
-    id: 'app.components.SelectDatabase.values',
-    select_form: 'Select Form',
-    target: 'Target',
-    target_id: 'Target ID',
-    experiment: 'Experiment',
-    chemical: 'Chemical',
-    toxicity: 'Toxicity',
-    assay: 'Assay',
-    citation: 'Citation',
-    citation_id: 'Citation ID',
+  select_form: {
+    id: 'app.components.SelectDatabase.select_form',
+    defaultMessage: 'Select Form',
+  },
+  target: {
+    id: 'app.components.SelectDatabase.target',
+    defaultMessage: 'Target',
+
+  },
+  target_id: {
+    id: 'app.components.SelectDatabase.target_id',
+    defaultMessage: 'Target ID',
+
+  },
+  experiment: {
+    id: 'app.components.SelectDatabase.experiment',
+    defaultMessage: 'Experiment',
+
+  },
+  chemical: {
+    id: 'app.components.SelectDatabase.chemical',
+    defaultMessage: 'Chemical',
+
+  },
+  toxicity: {
+    id: 'app.components.SelectDatabase.toxicity',
+    defaultMessage: 'Toxicity',
+
+  },
+  assay: {
+    id: 'app.components.SelectDatabase.assay',
+    defaultMessage: 'Assay',
+
+  },
+  citation: {
+    id: 'app.components.SelectDatabase.citation',
+    defaultMessage: 'Citation',
+
+  },
+  citation_id: {
+    id: 'app.components.SelectDatabase.citation_id',
+    defaultMessage: 'Citation ID',
   },
 });
