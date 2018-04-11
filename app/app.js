39e348d726ffc829a52f6c992e387d500cc25096
Will Jackson <wrejcodes@gmail.com>
2018-04-11 00:58:51 -0500

    Added bootstrap to the project :)

--- app/app.js
+++ app/app.js
@@ -15,6 +15,8 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import 'sanitize.css/sanitize.css';
+import '!!style-loader!css-loader!../vendor/css/bootstrap.min.css';
+
 
 // Import root app
 import App from 'containers/App';
